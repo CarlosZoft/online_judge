fbbc4bffdeac5e811255dd605c95d32f4f980e05
Carlos <carlosoftwar@gmail.com>
2021-01-18 19:41:42 -0300

    min and max sum in js

---
+++ Algorithms/Mini_MaxSum.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the miniMaxSum function below.
+function miniMaxSum(arr) {
+    var smaller = arr[0];
+    var bigger = arr[0];
+    for(var i in arr){
+        if(arr[i] < smaller) smaller = arr[i];
+        else if(arr[i] > bigger) bigger = arr[i];
+    }
+    var sum1 = (prev, next) => prev+next;
+    
+    var sum = arr.reduce(sum1);
+    console.log(`${sum - bigger} ${sum - smaller}`);
+
+}
+
+function main() {
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}

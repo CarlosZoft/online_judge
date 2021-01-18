5ce4f5a2785532bb4e29377b7b00a4a868d46381
Carlos <carlosoftwar@gmail.com>
2021-01-18 20:16:03 -0300

    Birthday Cake Candles

---
+++ Algorithms/birthdayCakeCandles.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    var candlesSort = candles.sort((a, b) => {
+        return a - b;
+    });
+    var sum = 0, result = 1;
+    for(var i=0; i<candles.length; i++) {
+        if(candlesSort[i]==candlesSort[i+1])sum++;
+        else {
+            if(sum > result) result = sum;
+            sum = 0;
+        }
+}
+ return result+1;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}

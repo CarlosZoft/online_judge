7527dbcfb3a534d6cfd8926d388f84744071cceb
Carlos Rafael <carlosk2gunn@hotmail.com>
2023-09-16 15:40:30 -0300

    ♻️ chore: removing redundant if

--- leetcode/power-of-three.cpp
+++ leetcode/power-of-three.cpp
@@ -4,19 +4,15 @@ public:
   bool isPowerOfThree(int n)
   {
     long long int total = 3;
-    if (n == 1)
+
+    if (n == 1 || n == total)
       return true;
-    else if (n < total)
+    if (n < total)
       return false;
-    else if (n == total)
-      return true;
+
     while (total < n)
-    {
       total *= 3;
-    }
-    if (total == n)
-      return true;
-    else
-      return false;
+
+    return total == n;
   }
 };

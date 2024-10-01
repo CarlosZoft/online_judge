259aca5114f9cb47ecfd05b535cac9543570a056
CarlosZoft <carlosk2gunn@hotmail.com>
2024-09-30 22:26:45 -0300

    Add codeforces template

---
+++ codeforces/template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, double> pii;
+typedef vector<int> vi;
+
+int main(){}
